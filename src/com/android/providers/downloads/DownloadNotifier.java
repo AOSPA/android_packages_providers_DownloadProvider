d93daa46584dcd9c9299ae52b482ef1e206c30ab
Lasse Brudeskar Vikås <bvx89.at.cs@gmail.com>
2014-03-04 20:32:22 +0100

    DownloadNotifier: Normal notification for singles
    This builds upon the previous DownloadNotifier, but changes the notification style
    when downloading single files by removing the big view.
    See this post for more information:
    https://plus.google.com/u/0/+Mart%C3%ADndelRosarioOK/posts/hgGcgsFqyp6
    
    Change-Id: Iaab38d7e7f9de3d9eb3deb5f061ee646b346c7cc

--- src/com/android/providers/downloads/DownloadNotifier.java
+++ src/com/android/providers/downloads/DownloadNotifier.java
@@ -60,6 +60,11 @@ public class DownloadNotifier {
     private static final int TYPE_WAITING = 2;
     private static final int TYPE_COMPLETE = 3;
 
+    private static final int SPEED_KB = 1024;
+    private static final int SPEED_MB = 1048576;
+    private static final int SPEED_GB = 1073741824;
+	
+
     private final Context mContext;
     private final NotificationManager mNotifManager;
 
@@ -89,7 +94,9 @@ public class DownloadNotifier {
     /**
      * Formatter for giving transfer speeds with maximum of one decimal places
      */
-    private final DecimalFormat mFormatter = new DecimalFormat("#.#");
+    private static final DecimalFormat mFormatter = new DecimalFormat("#.#");
+
+    private static final String SPEED_PLACEHOLDER = "%s %cB/s";
 
     public DownloadNotifier(Context context) {
         mContext = context;
@@ -224,26 +231,33 @@ public class DownloadNotifier {
                 }
 
                 if (total > 0) {
-                    final int percent = (int) ((current * 100) / total);
+                	final int percent = (int) ((current * 100) / total);
                     percentText = res.getString(R.string.download_percent, percent);
 
                     if (speed > 0) {
-                        // Determine postfix for download speed (B/s, KB/s or MB/s)
-                        String postFix = null;
-                        double speedNormalized = 0.0;
+                    	// Decide prefix character for speed string
+                    	char preFix;
+                    	double speedNormalized = speed;
 
-                        if (speed < 1024) {
-                            postFix = " B/s";
-                            speedNormalized = speed;
-                        } else if (speed < 1048576) {
-                            postFix = " KB/s";
-                            speedNormalized = (double)speed / 1024.0;
-                        } else if (speed < 1073741824) {
-                            postFix = " MB/s";
-                            speedNormalized = (double)speed / 1048576.0;
+                    	if (speed < SPEED_KB) {
+                    		preFix = '\0';
+                    	} else if (speed < SPEED_MB) {
+                    		preFix = 'K';
+                    		speedNormalized /= SPEED_KB;
+                    	} else if (speed < SPEED_GB) {
+                    		preFix = 'M';
+                    		speedNormalized /= SPEED_MB;
+                    	} else {
+				            preFix = 'G';
+                            speedNormalized /= SPEED_GB;
                         }
 
-                        speedText = mFormatter.format(speedNormalized) + postFix;
+                        // Format the String
+                    	speedText = String.format(
+                            SPEED_PLACEHOLDER,
+                            mFormatter.format(speedNormalized).toString(),
+                            preFix
+                        );
 
                         final long remainingMillis = ((total - current) * 1000) / speed;
                         remainingText = res.getString(R.string.download_remaining,
@@ -259,32 +273,23 @@ public class DownloadNotifier {
             // Build titles and description
             final Notification notif;
             if (cluster.size() == 1) {
-                final Notification.InboxStyle inboxStyle = new Notification.InboxStyle(builder);
 
                 final DownloadInfo info = cluster.iterator().next();
 
-                final String filename = getDownloadTitle(res, info).toString();
-
-                inboxStyle.addLine(filename);
-                builder.setContentTitle(filename);
+                builder.setContentTitle(getDownloadTitle(res, info));
 
                 String contentText = null;
 
-                if (type == TYPE_ACTIVE) {
-                    if (!TextUtils.isEmpty(info.mDescription)) {
+                if (type == TYPE_ACTIVE) {                    
+	                if (!TextUtils.isEmpty(info.mDescription)) {
                         builder.setContentText(info.mDescription);
-                    } else {
-                        builder.setContentText(remainingText);
                     }
-
-                    if (TextUtils.isEmpty(speedText) || TextUtils.isEmpty(remainingText)) {
-                        contentText = res.getString(R.string.download_running);
+                    
+                    if (speedText != null) {               
+                        builder.setContentInfo(speedText + ", " + percentText);                    
                     } else {
-                        contentText = speedText + ", " + remainingText;
+                        builder.setContentInfo(percentText);
                     }
-
-                    inboxStyle.setSummaryText(contentText);
-                    builder.setContentInfo(percentText);
 
                 } else if (type == TYPE_WAITING) {
                     builder.setContentText(
@@ -292,14 +297,18 @@ public class DownloadNotifier {
 
                 } else if (type == TYPE_COMPLETE) {
                     if (Downloads.Impl.isStatusError(info.mStatus)) {
-                        builder.setContentText(res.getText(R.string.notification_download_failed));
+
+                        contentText = res.getString(R.string.notification_download_failed);
+
+                        builder.setContentText(contentText);
                     } else if (Downloads.Impl.isStatusSuccess(info.mStatus)) {
-                        builder.setContentText(
-                                res.getText(R.string.notification_download_complete));
+                        contentText = res.getString(R.string.notification_download_complete);
+
+                        builder.setContentText(contentText);
                     }
                 }
 
-                notif = inboxStyle.build();
+                notif = builder.build();
 
             } else {
                 final Notification.InboxStyle inboxStyle = new Notification.InboxStyle(builder);
